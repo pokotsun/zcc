ea4e8ec9aedc374fbe6d55f1b1b153e5d4dae1e9
pokotsun <gorigolila@gmail.com>
2021-03-08 16:43:35 +0900

    implement GNU stmt_expr codegen

--- src/codegen.rs
+++ src/codegen.rs
@@ -32,32 +32,6 @@ fn arg_reg64(idx: usize) -> &'static str {
     reg_access(ARG_REGISTERS64, idx)
 }
 
-fn gen_addr(node: &Node, top: usize) -> Result<usize, String> {
-    match &node.kind {
-        NodeKind::Var { var } => {
-            match var.var_ty.clone() {
-                VarType::Local(offset) => {
-                    println!("  lea -{}(%rbp), {}", offset.get(), reg(top));
-                }
-                VarType::Global(_) => {
-                    println!("  mov ${}, {}", var.name, reg(top));
-                    // このコンパイラの場合, マイナス方向にデータが生えるため
-                    // 最初のデータまでのoffsetをとってやる
-                    // if let TypeKind::Arr { .. } = var.ty.kind.as_ref() {
-                    //     println!("  add ${}, {}", var.ty.size - 1, reg(top));
-                    // }
-                }
-            }
-            Ok(top + 1)
-        }
-        NodeKind::Unary(UnaryOp::Deref, child) => {
-            let top = gen_expr(child, top);
-            Ok(top)
-        }
-        _ => Err(format!("NodeKind is Invalid: {:?}, expected Var", node)),
-    }
-}
-
 fn load(ty: Type, top: usize) {
     let r = reg(top - 1);
     match ty.kind.as_ref() {
@@ -93,190 +67,6 @@ fn store(ty: Type, top: usize) -> usize {
     top - 1
 }
 
-fn gen_expr(node: &Node, mut top: usize) -> usize {
-    match &node.kind {
-        NodeKind::Num(val) => {
-            println!("  mov ${}, {}", val, reg(top));
-            top += 1;
-        }
-        NodeKind::Bin {
-            op: BinOp::Assign,
-            lhs,
-            rhs,
-        } => {
-            if let TypeKind::Arr { .. } = node.get_type().kind.as_ref() {
-                unimplemented!("array will not be assigned.")
-            }
-            top = gen_expr(&*rhs, top);
-            top = gen_addr(&*lhs, top).unwrap();
-            top = store(node.get_type(), top);
-        }
-        NodeKind::Bin { op, lhs, rhs } => {
-            top = gen_expr(&*rhs, gen_expr(&*lhs, top));
-            let rd = reg(top - 2);
-            let rs = reg(top - 1);
-            top -= 1;
-
-            match op {
-                BinOp::Add => println!("  add {}, {}", rs, rd),
-                BinOp::Sub => println!("  sub {}, {}", rs, rd),
-                BinOp::Mul => println!("  imul {}, {}", rs, rd),
-                BinOp::Div => {
-                    println!("  mov {}, %rax", rd);
-                    println!("  cqo");
-                    println!("  idiv {}", rs);
-                    println!("  mov %rax, {}", rd);
-                }
-                BinOp::Equal => {
-                    println!("  cmp {}, {}", rs, rd);
-                    println!("  sete %al");
-                    println!("  movzb %al, {}", rd);
-                }
-                BinOp::NEqual => {
-                    println!("  cmp {}, {}", rs, rd);
-                    println!("  setne %al");
-                    println!("  movzb %al, {}", rd);
-                }
-                BinOp::Lt => {
-                    println!("  cmp {}, {}", rs, rd);
-                    println!("  setl %al");
-                    println!("  movzb %al, {}", rd);
-                }
-                BinOp::Le => {
-                    println!("  cmp {}, {}", rs, rd);
-                    println!("  setle %al");
-                    println!("  movzb %al, {}", rd);
-                }
-                _ => error("Invalid BinOp, probably Assign"),
-            }
-        }
-        NodeKind::Var { .. } => {
-            top = gen_addr(&node, top).unwrap();
-            load(node.get_type(), top);
-        }
-        NodeKind::Unary(UnaryOp::Deref, child) => {
-            top = gen_expr(child, top);
-            load(node.get_type(), top);
-        }
-        NodeKind::Unary(UnaryOp::Addr, child) => {
-            top = gen_addr(child, top).unwrap();
-        }
-        // NodeKind::Unary(UnaryOp::StmtExpr, child) => {
-        //     if let NodeKind::Block(nodes) = &child.kind {
-        //         for node in nodes.iter() {
-        //             gen_stmt(node, label_counter, func, top)
-        //         }
-        //         top+=1;
-        //     }
-        // }
-        NodeKind::FunCall { name, args } => {
-            let nargs = args.len();
-            args.iter().for_each(|arg| top = gen_expr(arg, top));
-
-            for i in 1..=nargs {
-                top -= 1;
-                println!("  mov {}, {}", reg(top), arg_reg64(nargs - i));
-            }
-
-            // 何故reg11もpushする必要がある?
-            println!("  push %r10");
-            println!("  push %r11");
-            println!("  mov $0, %rax");
-            println!("  call {}", name);
-            println!("  pop %r11");
-            println!("  pop %r10");
-            println!("  mov %rax, {}", reg(top));
-            top += 1;
-        }
-        _ => {
-            error(&format!("invalid expression: {:?}", node));
-        }
-    }
-    top
-}
-
-// return register top index
-fn gen_stmt(
-    node: &Node,
-    label_counter: &mut LabelCounter,
-    func: &Function,
-    top: usize,
-) -> Result<usize, String> {
-    let stack_top = match &node.kind {
-        NodeKind::Unary(UnaryOp::Return, child) => {
-            let top = gen_expr(child, top) - 1;
-            // Set the result of the expression to RAX so that
-            // the result becomes a return value of this function.
-            println!("  mov {}, %rax", reg(top));
-            println!("  jmp .L.return.{}", func.name);
-
-            Ok(top)
-        }
-        NodeKind::Unary(UnaryOp::ExprStmt, child) => Ok(gen_expr(child, top) - 1),
-        NodeKind::If { cond, then, els } => {
-            let c = label_counter.next().unwrap();
-            let mut top = gen_expr(cond, top);
-            println!("  cmp $0, {}", reg(top - 1));
-            top -= 1;
-            println!("  je .L.else.{}", c);
-            top = gen_stmt(then, label_counter, func, top)?;
-            println!("  jmp .L.end.{}", c);
-            println!(".L.else.{}:", c);
-
-            if let Some(node) = els.as_ref() {
-                top = gen_stmt(node, label_counter, func, top)?;
-            }
-            println!(".L.end.{}:", c);
-            Ok(top)
-        }
-        NodeKind::For {
-            cond,
-            then,
-            init,
-            inc,
-        } => {
-            let c = label_counter.next().unwrap();
-            let mut top = gen_stmt(init, label_counter, func, top)?;
-            println!(".L.begin.{}:", c);
-            if let Some(node) = cond.as_ref() {
-                top = gen_expr(node, top);
-                println!("  cmp $0, {}", reg(top - 1));
-                top -= 1;
-                println!("  je .L.end.{}", c);
-            }
-            top = gen_stmt(then, label_counter, func, top)?;
-            if let Some(node) = inc.as_ref() {
-                top = gen_expr(node, top);
-                top -= 1;
-            }
-            println!("  jmp .L.begin.{}", c);
-            println!(".L.end.{}:", c);
-            Ok(top)
-        }
-        NodeKind::While { cond, then } => {
-            let c = label_counter.next().unwrap();
-            println!(".L.begin.{}:", c);
-            let mut top = gen_expr(cond, top);
-            println!("  cmp $0, {}", reg(top - 1));
-            top -= 1;
-            println!("  je .L.end.{}", c);
-            top = gen_stmt(then, label_counter, func, top)?;
-            println!("  jmp .L.begin.{}", c);
-            println!(".L.end.{}:", c);
-            Ok(top)
-        }
-        NodeKind::Block(body) => body.iter().fold(Ok(top), |acc, node| {
-            acc.and_then(|x| gen_stmt(&node, label_counter, func, top).and_then(|y| Ok(x + y)))
-        }),
-        _ => Err(format!("invalid statement: {:?}", node)),
-    };
-    match stack_top {
-        Ok(0) => Ok(0),
-        Ok(_) => Err(format!("statement register top is invalid: {:?}", node)),
-        Err(msg) => Err(msg),
-    }
-}
-
 fn emit_data(prog: &Program) {
     println!(".data");
     for var in prog.globals.iter() {
@@ -293,11 +83,225 @@ fn emit_data(prog: &Program) {
     }
 }
 
+struct FuncGenerator<'a> {
+    func: &'a Function,
+    label_counter: &'a mut LabelCounter,
+}
+
+impl<'a> FuncGenerator<'a> {
+    fn new(func: &'a Function, label_counter: &'a mut LabelCounter) -> Self {
+        Self {
+            func: func,
+            label_counter,
+        }
+    }
+    fn gen_addr(&mut self, node: &Node, top: usize) -> Result<usize, String> {
+        match &node.kind {
+            NodeKind::Var { var } => {
+                match var.var_ty.clone() {
+                    VarType::Local(offset) => {
+                        println!("  lea -{}(%rbp), {}", offset.get(), reg(top));
+                    }
+                    VarType::Global(_) => {
+                        println!("  mov ${}, {}", var.name, reg(top));
+                    }
+                }
+                Ok(top + 1)
+            }
+            NodeKind::Unary(UnaryOp::Deref, child) => {
+                let top = self.gen_expr(child, top);
+                Ok(top)
+            }
+            _ => Err(format!("NodeKind is Invalid: {:?}, expected Var", node)),
+        }
+    }
+
+    #[allow(unused_must_use)]
+    fn gen_expr(&mut self, node: &Node, mut top: usize) -> usize {
+        match &node.kind {
+            NodeKind::Num(val) => {
+                println!("  mov ${}, {}", val, reg(top));
+                top += 1;
+            }
+            NodeKind::Bin {
+                op: BinOp::Assign,
+                lhs,
+                rhs,
+            } => {
+                if let TypeKind::Arr { .. } = node.get_type().kind.as_ref() {
+                    unimplemented!("array will not be assigned.")
+                }
+                top = self.gen_expr(&*rhs, top);
+                top = self.gen_addr(&*lhs, top).unwrap();
+                top = store(node.get_type(), top);
+            }
+            NodeKind::Bin { op, lhs, rhs } => {
+                top = self.gen_expr(&*lhs, top);
+                top = self.gen_expr(&*rhs, top);
+                let rd = reg(top - 2);
+                let rs = reg(top - 1);
+                top -= 1;
+
+                match op {
+                    BinOp::Add => println!("  add {}, {}", rs, rd),
+                    BinOp::Sub => println!("  sub {}, {}", rs, rd),
+                    BinOp::Mul => println!("  imul {}, {}", rs, rd),
+                    BinOp::Div => {
+                        println!("  mov {}, %rax", rd);
+                        println!("  cqo");
+                        println!("  idiv {}", rs);
+                        println!("  mov %rax, {}", rd);
+                    }
+                    BinOp::Equal => {
+                        println!("  cmp {}, {}", rs, rd);
+                        println!("  sete %al");
+                        println!("  movzb %al, {}", rd);
+                    }
+                    BinOp::NEqual => {
+                        println!("  cmp {}, {}", rs, rd);
+                        println!("  setne %al");
+                        println!("  movzb %al, {}", rd);
+                    }
+                    BinOp::Lt => {
+                        println!("  cmp {}, {}", rs, rd);
+                        println!("  setl %al");
+                        println!("  movzb %al, {}", rd);
+                    }
+                    BinOp::Le => {
+                        println!("  cmp {}, {}", rs, rd);
+                        println!("  setle %al");
+                        println!("  movzb %al, {}", rd);
+                    }
+                    _ => error("Invalid BinOp, probably Assign"),
+                }
+            }
+            NodeKind::Var { .. } => {
+                top = self.gen_addr(&node, top).unwrap();
+                load(node.get_type(), top);
+            }
+            NodeKind::Unary(UnaryOp::Deref, child) => {
+                top = self.gen_expr(child, top);
+                load(node.get_type(), top);
+            }
+            NodeKind::Unary(UnaryOp::Addr, child) => {
+                top = self.gen_addr(child, top).unwrap();
+            }
+            NodeKind::Unary(UnaryOp::StmtExpr, child) => {
+                if let NodeKind::Block(nodes) = &child.kind {
+                    for node in nodes.iter() {
+                        self.gen_stmt(node, top);
+                    }
+                    top += 1;
+                }
+            }
+            NodeKind::FunCall { name, args } => {
+                let nargs = args.len();
+                args.iter().for_each(|arg| top = self.gen_expr(arg, top));
+
+                for i in 1..=nargs {
+                    top -= 1;
+                    println!("  mov {}, {}", reg(top), arg_reg64(nargs - i));
+                }
+
+                // 何故reg11もpushする必要がある?
+                println!("  push %r10");
+                println!("  push %r11");
+                println!("  mov $0, %rax");
+                println!("  call {}", name);
+                println!("  pop %r11");
+                println!("  pop %r10");
+                println!("  mov %rax, {}", reg(top));
+                top += 1;
+            }
+            _ => {
+                error(&format!("invalid expression: {:?}", node));
+            }
+        }
+        top
+    }
+
+    // return register top index
+    fn gen_stmt(&mut self, node: &Node, top: usize) -> Result<usize, String> {
+        let stack_top = match &node.kind {
+            NodeKind::Unary(UnaryOp::Return, child) => {
+                let top = self.gen_expr(child, top) - 1;
+                // Set the result of the expression to RAX so that
+                // the result becomes a return value of this function.
+                println!("  mov {}, %rax", reg(top));
+                println!("  jmp .L.return.{}", self.func.name);
+
+                Ok(top)
+            }
+            NodeKind::Unary(UnaryOp::ExprStmt, child) => Ok(self.gen_expr(child, top) - 1),
+            NodeKind::If { cond, then, els } => {
+                let c = self.label_counter.next().unwrap();
+                let mut top = self.gen_expr(cond, top);
+                println!("  cmp $0, {}", reg(top - 1));
+                top -= 1;
+                println!("  je .L.else.{}", c);
+                top = self.gen_stmt(then, top)?;
+                println!("  jmp .L.end.{}", c);
+                println!(".L.else.{}:", c);
+
+                if let Some(node) = els.as_ref() {
+                    top = self.gen_stmt(node, top)?;
+                }
+                println!(".L.end.{}:", c);
+                Ok(top)
+            }
+            NodeKind::For {
+                cond,
+                then,
+                init,
+                inc,
+            } => {
+                let c = self.label_counter.next().unwrap();
+                let mut top = self.gen_stmt(init, top)?;
+                println!(".L.begin.{}:", c);
+                if let Some(node) = cond.as_ref() {
+                    top = self.gen_expr(node, top);
+                    println!("  cmp $0, {}", reg(top - 1));
+                    top -= 1;
+                    println!("  je .L.end.{}", c);
+                }
+                top = self.gen_stmt(then, top)?;
+                if let Some(node) = inc.as_ref() {
+                    top = self.gen_expr(node, top);
+                    top -= 1;
+                }
+                println!("  jmp .L.begin.{}", c);
+                println!(".L.end.{}:", c);
+                Ok(top)
+            }
+            NodeKind::While { cond, then } => {
+                let c = self.label_counter.next().unwrap();
+                println!(".L.begin.{}:", c);
+                let mut top = self.gen_expr(cond, top);
+                println!("  cmp $0, {}", reg(top - 1));
+                top -= 1;
+                println!("  je .L.end.{}", c);
+                top = self.gen_stmt(then, top)?;
+                println!("  jmp .L.begin.{}", c);
+                println!(".L.end.{}:", c);
+                Ok(top)
+            }
+            NodeKind::Block(body) => body.iter().fold(Ok(top), |acc, node| {
+                acc.and_then(|x| self.gen_stmt(&node, top).and_then(|y| Ok(x + y)))
+            }),
+            _ => Err(format!("invalid statement: {:?}", node)),
+        };
+        match stack_top {
+            Ok(0) => Ok(0),
+            Ok(_) => Err(format!("statement register top is invalid: {:?}", node)),
+            Err(msg) => Err(msg),
+        }
+    }
+}
+
 fn emit_text(prog: &Program) {
     println!(".text");
 
-    let mut label_counter = LabelCounter::new();
-
+    let mut org_label_counter = LabelCounter::new();
     for func in prog.fns.iter() {
         println!(".globl {}", func.name);
         println!("{}:", func.name);
@@ -330,8 +334,11 @@ fn emit_text(prog: &Program) {
             }
         }
 
+        let mut fn_gen = FuncGenerator::new(func, &mut org_label_counter);
+
         // Emit code
-        if let Err(msg) = gen_stmt(&func.body, &mut label_counter, &func, 0) {
+        // if let Err(msg) = gen_stmt(&func.body, &mut label_counter, &func, 0) {
+        if let Err(msg) = fn_gen.gen_stmt(&fn_gen.func.body, 0) {
             error(&msg);
         }
 
