974f218658fb524875d28d428d09b60b11ae5658
pokotsun <gorigolila@gmail.com>
2021-04-03 16:47:51 +0900

    Read code from a file instead of argv[1]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 use std::iter::Iterator;
 use std::process;
 use zcc::codegen::*;
@@ -12,15 +14,26 @@ use zcc::tokenize::*;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
+    if args.len() != 2 && args.len() != 3 {
         eprintln!("{}: invalid number of arguments", args[0]);
         process::exit(1);
     }
 
     // Tokenize and parse.
-    let chars = args[1].clone();
+    let read_symbol = args[1].clone();
 
-    let tokens = tokenize(chars)?;
+    // By convention, read from stdin if a given filename is "-".
+    let contents = match &*read_symbol {
+        "-" => args[2].to_string(),
+        _ => {
+            let mut file = File::open(args[1].clone())?;
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)?;
+            contents
+        }
+    };
+
+    let tokens = tokenize(contents)?;
     let tok_peek = tokens.iter().peekable();
 
     let prog = Parser::parse(tok_peek);
